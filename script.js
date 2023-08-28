b424fa5614596135350107707ddc4d32d68cc958
yonasFeshaye <yonas.m.feshaye@hotmail.com>
2023-08-28 16:49:34 -0400

    added more elements, styles and scripts

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+// Event Listnner
+toggleSwitch.addEventListener("change", switchTheme);
+
 
